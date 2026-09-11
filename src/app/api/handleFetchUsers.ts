d19dd5ca482208d171b3261b30b0855b08e4c527
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2026-09-11 21:52:49 +0200

    cache: 'no-store',

--- src/app/api/handleFetchUsers.ts
+++ src/app/api/handleFetchUsers.ts
@@ -7,6 +7,7 @@ export const handleFetchUsers = async () => {
             headers: {
                 'Content-Type': 'application/json',
             },
+            cache: 'no-store',
         })
 
         if (!response.ok) {
